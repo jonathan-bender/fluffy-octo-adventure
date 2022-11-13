a1f83d5fe20f112964435137e776ac735dfe8141
Jonathan Bender <jonathanbender82@gmail.com>
2022-11-13 12:29:19 +0200

    reading files fixed

--- readProfilometerData.m
+++ readProfilometerData.m
@@ -1,14 +1,14 @@
 
 % consts
-PATH='C:\Users\Owner\Desktop\Profile\202209-PVC\LowerBlock\2022-09-25_19-02-35';
-PLOT_POINTS = 5000;
+PATH='C:\Profilometer_Data\2022-10-19_23-15-17';
+PLOT_POINTS = 500000;
 
 % read data
 opts = delimitedTextImportOptions('NumVariables', 5);
 opts.DataLines = [2, Inf];
 opts.Delimiter = '\t';
-opts.VariableNames = ['Counter', 'Altitudem', 'Encoder1m', 'Encoder2m', 'RowNumber'];
-opts.VariableTypes = ['double', 'double', 'double', 'double', 'double'];
+opts.VariableNames = {'Counter', 'Altitudem', 'Encoder1m', 'Encoder2m', 'RowNumber'};
+opts.VariableTypes = {'double', 'double', 'double', 'double', 'double'};
 opts.ExtraColumnsRule = 'ignore';
 opts.EmptyLineRule = 'read';
 tableData = readtable([PATH '.txt'], opts);
@@ -18,12 +18,12 @@ data = table2array(tableData);
 paramOpts = delimitedTextImportOptions('NumVariables', 2);
 paramOpts.DataLines = [3, Inf];
 paramOpts.Delimiter = '=';
-paramOpts.VariableNames = ['Frequency', 'Hz'];
-paramOpts.VariableTypes = ['double', 'double'];
+paramOpts.VariableNames = {'Frequency', 'Hz'};
+paramOpts.VariableTypes = {'double', 'double'};
 paramOpts.ExtraColumnsRule = 'ignore';
 paramOpts.EmptyLineRule = 'read';
-paramOpts = setvaropts(paramOpts, ['Frequency', 'Hz'], 'TrimNonNumeric', true);
-paramOpts = setvaropts(paramOpts, ['Frequency', 'Hz'], 'ThousandsSeparator', ',');
+paramOpts = setvaropts(paramOpts, {'Frequency', 'Hz'}, 'TrimNonNumeric', true);
+paramOpts = setvaropts(paramOpts, {'Frequency', 'Hz'}, 'ThousandsSeparator', ',');
 tableParameters = readtable([PATH '_parameters.txt'], paramOpts);
 parameters = table2array(tableParameters);
 
@@ -40,7 +40,12 @@ read = zeros(rowCount, columnCount);
 currentRead = 1;
 for i=1:rowCount
     for j=1:columnCount
-        read(i,j) = data(currentRead,2);
+        readColumn = j;
+        if rem(i,2) == 0
+            readColumn = columnCount - j + 1;
+        end
+        
+        read(i,readColumn) = data(currentRead,2);
         currentRead = currentRead+1;
     end
 end
@@ -49,8 +54,8 @@ end
 % plot height
 currentPoints = size(read,1) * size(read,2);
 if currentPoints > PLOT_POINTS
-    resizeRatio = round(sqrt(currentPoints / PLOT_POINTS));
-    read = resize(read, resizeRatio);
+    resizeRatio = 1 / sqrt(currentPoints / PLOT_POINTS);
+    read = imresize(read, resizeRatio);
 end
 
 image(read);
